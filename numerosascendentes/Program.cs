bdb8f4d46a7cf27aa14050f3142a8a26d100048c
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:36:26 -0500

    Agregar archivos de proyecto.

---
+++ numerosascendentes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerosascendentes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }using System;
+
+class NumerosAscendentes
+    {
+        static void Main()
+        {
+            int i;
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+}
